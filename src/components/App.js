07bd3bddf1b2fb59c85bcaf4934ae4c810ada73d
Erik Luebbers <erluebbers@gmail.com>
2022-01-16 12:34:05 -0800

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,28 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleFetch() {
+    fetch("http://localhost:3001/toys")
+      .then(r => r.json())
+      .then(data => setToys(data))
+  }
+
+  useEffect(handleFetch, [])
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm handleFetch={handleFetch} setToys={setToys}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer handleFetch={handleFetch} setToys={setToys} toys={toys}/>
     </>
   );
 }
